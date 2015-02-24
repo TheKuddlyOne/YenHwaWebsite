e967fac7a4278e0a0518dae4fd3a2aa17fd66947
TheKuddlyOne <cgp46@cornell.edu>
2015-02-24 17:53:27 -0500

    Base homepage

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Yen Hwa Chinese Restaurant</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+
+</body>
+</html>
